709333474ca3f7b2e9195c921e323ce5ef6d0668
Irenee <icaroulle@gmail.com>
2021-06-21 21:18:24 +0200

    Add ucdp Config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use actix_web::{post, web, App, Error, HttpResponse, HttpServer};
-use isahc::{prelude::*, HttpClient};
+use isahc::prelude::ReadResponseExt;
 use std::collections::HashMap;
 
 mod ucdp;
@@ -24,32 +24,14 @@ async fn proxy(
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let mut config = config::Config::default();
-    config
-        .merge(config::File::with_name("config/Main"))
-        .unwrap();
+    let config = ucdp::Config::new(String::from("config/Main"));
 
-    let bind_config = config.get_str("server.bind").unwrap();
-
-    let clients_config = config.get_table("clients").unwrap();
-    let clients = clients_config
-        .into_iter()
-        .map(|(client_id, address)| {
-            (
-                client_id.clone(),
-                Client {
-                    id: client_id.clone(),
-                    address: address.into_str().unwrap(),
-                    client: HttpClient::new().unwrap(),
-                },
-            )
-        })
-        .collect();
-
-    let state = web::Data::new(AppState { clients: clients });
+    let state = web::Data::new(AppState {
+        clients: config.get_clients(),
+    });
 
     HttpServer::new(move || App::new().app_data(state.clone()).service(proxy))
-        .bind(bind_config)?
+        .bind(config.get_server_binding_address())?
         .run()
         .await
 }
--- src/ucdp/mod.rs
+++ src/ucdp/mod.rs
@@ -1,5 +1,41 @@
+use isahc::HttpClient;
+use std::collections::HashMap;
+
 pub struct Client {
     pub id: String,
     pub address: String,
     pub client: isahc::HttpClient,
+}
+
+pub struct Config {
+    config: config::Config,
+}
+
+impl Config {
+    pub fn new(path: String) -> Self {
+        let mut config = config::Config::default();
+        config.merge(config::File::with_name(&path)).unwrap();
+        return Config { config: config };
+    }
+
+    pub fn get_server_binding_address(&self) -> String {
+        return self.config.get_str("server.bind").unwrap();
+    }
+
+    pub fn get_clients(&self) -> HashMap<String, Client> {
+        let clients_config = self.config.get_table("clients").unwrap();
+        return clients_config
+            .into_iter()
+            .map(|(client_id, address)| {
+                (
+                    client_id.clone(),
+                    Client {
+                        id: client_id.clone(),
+                        address: address.into_str().unwrap(),
+                        client: HttpClient::new().unwrap(),
+                    },
+                )
+            })
+            .collect();
+    }
 }
