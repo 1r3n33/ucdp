3990a2e8ccdc91e3c7b95238452192707c3b1e93
Irenee <icaroulle@gmail.com>
2021-06-23 18:45:38 +0200

    Add Client tests

--- src/ucdp/mod.rs
+++ src/ucdp/mod.rs
@@ -1,6 +1,7 @@
 use isahc::HttpClient;
 use std::collections::HashMap;
 
+#[derive(Debug)]
 pub struct Client {
     pub id: String,
     pub address: String,
@@ -37,5 +38,53 @@ impl Config {
                 )
             })
             .collect();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    impl PartialEq for Client {
+        fn eq(&self, other: &Self) -> bool {
+            self.id == other.id && self.address == other.address
+        }
+    }
+
+    fn config() -> Config {
+        let mut config = config::Config::default();
+        config.set("server.bind", "0.0.0.0:0000").unwrap();
+        config.set("clients.first", "1.1.1.1:1111").unwrap();
+        config.set("clients.second", "2.2.2.2:2222").unwrap();
+        return Config { config: config };
+    }
+
+    #[test]
+    fn config_get_server_binding_address() {
+        assert_eq!(config().get_server_binding_address(), "0.0.0.0:0000");
+    }
+
+    #[test]
+    fn config_get_clients() {
+        let mut expected = HashMap::new();
+        expected.insert(
+            String::from("first"),
+            Client {
+                id: String::from("first"),
+                address: String::from("1.1.1.1:1111"),
+                client: HttpClient::new().unwrap(),
+            },
+        );
+        expected.insert(
+            String::from("second"),
+            Client {
+                id: String::from("second"),
+                address: String::from("2.2.2.2:2222"),
+                client: HttpClient::new().unwrap(),
+            },
+        );
+
+        let clients = config().get_clients();
+        assert_eq!(clients, expected);
     }
 }
