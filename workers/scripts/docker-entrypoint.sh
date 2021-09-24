c439d3a753e011d289a2e2610b016f080caadb48
Irenee <icaroulle@gmail.com>
2021-09-24 17:40:15 +0200

    Dockerize workers

---
+++ workers/scripts/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Abort on any error (including if wait-for-it fails).
+set -e
+
+# Wait for dependencies to be up, if we know where they are.
+if [ -n "$UCDP_STREAM_KAFKA_BROKER" ]; then
+  /ucdp/workers/scripts/wait-for-it.sh -t 30 "$UCDP_STREAM_KAFKA_BROKER"
+fi
+
+# Run the main container command.
+exec "$@"
