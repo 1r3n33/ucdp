ea5e597f23067197812b3e07392148d834013a59
Irenee <icaroulle@gmail.com>
2021-10-11 19:28:21 +0200

    Add tests

--- gateway/src/ucdp/dal/authorized_partners_by_user.rs
+++ gateway/src/ucdp/dal/authorized_partners_by_user.rs
@@ -1,4 +1,4 @@
-use crate::ucdp::dal::ethereum_dao::{EthereumDao, EthereumDaoBuilder, EthereumDaoError};
+use crate::ucdp::dal::ethereum_dao::{EthereumContractQuery, EthereumDaoBuilder, EthereumDaoError};
 use async_trait::async_trait;
 use std::fmt::Debug;
 use std::str::FromStr;
@@ -25,12 +25,13 @@ pub trait AuthorizedPartnersByUserDao: Send + Sync {
     async fn is_authorized(&self, user_id: &str, partner_id: &str) -> Result<bool, Error>;
 }
 
-struct AuthorizedPartnersByUserEthereumDao {
-    ethereum_dao: EthereumDao<(web3::types::Address, web3::types::Address), bool>,
+struct AuthorizedPartnersByUserEthereumDao<'a> {
+    ethereum_dao:
+        Box<dyn EthereumContractQuery<'a, (web3::types::Address, web3::types::Address), bool>>,
 }
 
 #[async_trait]
-impl AuthorizedPartnersByUserDao for AuthorizedPartnersByUserEthereumDao {
+impl AuthorizedPartnersByUserDao for AuthorizedPartnersByUserEthereumDao<'_> {
     async fn is_authorized(&self, user_id: &str, partner_id: &str) -> Result<bool, Error> {
         let user_adress = web3::types::Address::from_str(user_id)
             .map_err(|_| Error::Parameter("user_id".into()))?;
@@ -81,5 +82,155 @@ impl AuthorizedPartnersByUserBuilderForTest {
         dao: Box<dyn AuthorizedPartnersByUserDao>,
     ) -> Result<AuthorizedPartnersByUser, Error> {
         Ok(AuthorizedPartnersByUser { dao })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Error;
+    use crate::ucdp::dal::authorized_partners_by_user::AuthorizedPartnersByUserEthereumDao;
+    use crate::ucdp::dal::ethereum_dao::{EthereumContractQuery, EthereumDaoError};
+    use crate::ucdp::dal::{AuthorizedPartnersByUser, AuthorizedPartnersByUserBuilder};
+    use async_trait::async_trait;
+    use ucdp::config::Config;
+
+    #[test]
+    fn authorized_partners_by_user_builder_build_ok() {
+        let mut config = config::Config::default();
+        let _ = config.set("data.authorized_partners_by_user.connector", "ethereum");
+        let _ = config.set("ethereum.network", "http://ethereum");
+        let _ = config.set(
+            "ethereum.contract",
+            "0x0000000000000000000000000000000000000000",
+        );
+        let config = Config::from(config);
+
+        let res = AuthorizedPartnersByUserBuilder::build(&config);
+        assert!(res.is_ok())
+    }
+
+    #[test]
+    fn authorized_partners_by_user_builder_build_err_missing_connector() {
+        let config = config::Config::default();
+        let config = Config::from(config);
+
+        let res = AuthorizedPartnersByUserBuilder::build(&config);
+        match res {
+            Err(Error::Config(_)) => (),
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn authorized_partners_by_user_builder_build_err_unknown_connector() {
+        let mut config = config::Config::default();
+        let _ = config.set("data.authorized_partners_by_user.connector", "unknown");
+        let config = Config::from(config);
+
+        let res = AuthorizedPartnersByUserBuilder::build(&config);
+        if let Err(Error::UnknownConnector(reason)) = res {
+            assert_eq!(reason, "unknown");
+        } else {
+            unreachable!();
+        }
+    }
+
+    pub struct OptionTestEthereumDao {
+        value: Option<bool>,
+    }
+
+    #[async_trait]
+    impl<'a> EthereumContractQuery<'a, (web3::types::Address, web3::types::Address), bool>
+        for OptionTestEthereumDao
+    {
+        async fn get(
+            &self,
+            _: (web3::types::Address, web3::types::Address),
+        ) -> Result<bool, EthereumDaoError> {
+            self.value.ok_or_else(|| {
+                EthereumDaoError::Execution(web3::contract::Error::InvalidOutputType(
+                    "error".into(),
+                ))
+            })
+        }
+    }
+
+    #[actix_rt::test]
+    async fn authorized_partners_by_user_is_authorized_ok() {
+        let ethereum_dao = OptionTestEthereumDao { value: Some(true) };
+        let dao = AuthorizedPartnersByUserEthereumDao {
+            ethereum_dao: Box::new(ethereum_dao),
+        };
+        let authorized_partners_by_user = AuthorizedPartnersByUser { dao: Box::new(dao) };
+
+        let res = authorized_partners_by_user
+            .is_authorized(
+                "0x0000000000000000000000000000000000000123",
+                "0x0000000000000000000000000000000000000456",
+            )
+            .await;
+        assert!(res.is_ok());
+    }
+
+    #[actix_rt::test]
+    async fn authorized_partners_by_user_is_authorized_err_contract() {
+        let ethereum_dao = OptionTestEthereumDao { value: None };
+        let dao = AuthorizedPartnersByUserEthereumDao {
+            ethereum_dao: Box::new(ethereum_dao),
+        };
+        let authorized_partners_by_user = AuthorizedPartnersByUser { dao: Box::new(dao) };
+
+        let res = authorized_partners_by_user
+            .is_authorized(
+                "0x0000000000000000000000000000000000000123",
+                "0x0000000000000000000000000000000000000456",
+            )
+            .await;
+        match res {
+            Err(Error::Contract(_)) => (),
+            _ => unreachable!(),
+        }
+    }
+
+    #[actix_rt::test]
+    async fn authorized_partners_by_user_is_authorized_err_parameter_user_id() {
+        let ethereum_dao = OptionTestEthereumDao { value: Some(true) };
+        let dao = AuthorizedPartnersByUserEthereumDao {
+            ethereum_dao: Box::new(ethereum_dao),
+        };
+        let authorized_partners_by_user = AuthorizedPartnersByUser { dao: Box::new(dao) };
+
+        let res = authorized_partners_by_user
+            .is_authorized(
+                "not an address",
+                "0x0000000000000000000000000000000000000456",
+            )
+            .await;
+        if let Err(Error::Parameter(reason)) = res {
+            assert_eq!(reason, "user_id");
+        } else {
+            unreachable!();
+        }
+    }
+
+    #[actix_rt::test]
+    async fn authorized_partners_by_user_is_authorized_err_parameter_partner_id() {
+        let ethereum_dao = OptionTestEthereumDao { value: Some(true) };
+        let dao = AuthorizedPartnersByUserEthereumDao {
+            ethereum_dao: Box::new(ethereum_dao),
+        };
+        let authorized_partners_by_user = AuthorizedPartnersByUser { dao: Box::new(dao) };
+
+        let res = authorized_partners_by_user
+            .is_authorized(
+                "0x0000000000000000000000000000000000000123",
+                "not an address",
+            )
+            .await;
+        if let Err(Error::Parameter(reason)) = res {
+            assert_eq!(reason, "partner_id");
+        } else {
+            unreachable!();
+        }
     }
 }
--- gateway/src/ucdp/dal/ethereum_dao.rs
+++ gateway/src/ucdp/dal/ethereum_dao.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use std::str::FromStr;
 use thiserror::Error;
 use ucdp::config::Config;
@@ -21,19 +22,28 @@ pub enum EthereumDaoError {
     Network(#[from] web3::Error),
 }
 
-pub struct EthereumDao<K, R> {
-    contract: web3::contract::Contract<web3::transports::Http>,
-    function_name: String,
-    _k: std::marker::PhantomData<K>,
-    _r: std::marker::PhantomData<R>,
+#[async_trait]
+pub trait EthereumContractQuery<'a, K, R>: Send + Sync {
+    async fn get(&self, key: K) -> Result<R, EthereumDaoError>
+    where
+        'a: 'async_trait;
 }
 
-impl<K, R> EthereumDao<K, R>
+pub struct EthereumDao {
+    contract: web3::contract::Contract<web3::transports::Http>,
+    function_name: String,
+}
+
+#[async_trait]
+impl<'a, K, R> EthereumContractQuery<'a, K, R> for EthereumDao
 where
-    K: Tokenize,
+    K: Tokenize + Send + 'a,
     R: Tokenizable,
 {
-    pub async fn get(&self, key: K) -> Result<R, EthereumDaoError> {
+    async fn get(&self, key: K) -> Result<R, EthereumDaoError>
+    where
+        'a: 'async_trait,
+    {
         self.contract
             .query(
                 self.function_name.as_str(),
@@ -52,11 +62,15 @@ pub struct EthereumDaoBuilder<K, R> {
     _r: std::marker::PhantomData<R>,
 }
 
-impl<K, R> EthereumDaoBuilder<K, R> {
+impl<'a, K, R> EthereumDaoBuilder<K, R>
+where
+    K: Tokenize + Send + 'a,
+    R: Tokenizable,
+{
     pub fn build(
         config: &Config,
         function_name: &str,
-    ) -> Result<EthereumDao<K, R>, EthereumDaoError> {
+    ) -> Result<Box<dyn EthereumContractQuery<'a, K, R>>, EthereumDaoError> {
         let network = config.get_str("ethereum.network")?;
         let contract_address = config
             .get_str("ethereum.contract")
@@ -74,10 +88,69 @@ impl<K, R> EthereumDaoBuilder<K, R> {
         let dao = EthereumDao {
             contract,
             function_name: function_name.into(),
-            _k: std::marker::PhantomData,
-            _r: std::marker::PhantomData,
         };
 
-        Ok(dao)
+        Ok(Box::new(dao))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::ucdp::dal::ethereum_dao::{EthereumDaoBuilder, EthereumDaoError};
+    use ucdp::config::Config;
+
+    fn config(network: Option<&str>, address: Option<&str>) -> Config {
+        let mut config = config::Config::default();
+        if let Some(network) = network {
+            let _ = config.set("ethereum.network", network);
+        }
+        if let Some(address) = address {
+            let _ = config.set("ethereum.contract", address);
+        }
+        Config::from(config)
+    }
+
+    #[test]
+    fn ethereum_dao_builder_build_ok() {
+        let config = config(
+            Some("http://ethereum"),
+            Some("0x0000000000000000000000000000000000000000"),
+        );
+        let res = EthereumDaoBuilder::<u32, u32>::build(&config, "function_name");
+        assert!(res.is_ok())
+    }
+
+    #[test]
+    fn ethereum_dao_builder_build_err_config() {
+        let config = config(None, None);
+        let res = EthereumDaoBuilder::<u32, u32>::build(&config, "function_name");
+        match res {
+            Err(EthereumDaoError::Config(_)) => (),
+            _ => unreachable!(),
+        }
+    }
+
+    #[test]
+    fn ethereum_dao_builder_build_err_parameter() {
+        let config = config(Some("http://ethereum"), Some("not an address"));
+        let res = EthereumDaoBuilder::<u32, u32>::build(&config, "function_name");
+        if let Err(EthereumDaoError::Parameter(reason)) = res {
+            assert_eq!(reason, "ethereum.contract");
+        } else {
+            unreachable!();
+        }
+    }
+
+    #[test]
+    fn ethereum_dao_builder_build_err_network() {
+        let config = config(
+            Some("not a network"),
+            Some("0x0000000000000000000000000000000000000000"),
+        );
+        let res = EthereumDaoBuilder::<u32, u32>::build(&config, "function_name");
+        match res {
+            Err(EthereumDaoError::Network(_)) => (),
+            _ => unreachable!(),
+        }
     }
 }
